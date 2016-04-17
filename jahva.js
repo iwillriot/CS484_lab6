6fd70c8f7c769029f7a87b0d671ae02026d69b4c
iwillriot <william.diaz.102@my.csun.edu>
2016-04-17 05:42:27 -0700

    calculations for total are done.

--- jahva.js
+++ jahva.js
@@ -1,10 +1,25 @@
 function run(){
 	var quantity = document.getElementById("quantity").value;
 	var ppunit = document.getElementById("ppunit").value;
+	var tax = document.getElementById("tax").value;
+	var discount = document.getElementById("discount").value;
+	
 	
 	ppunit = parseFloat(ppunit);
-	var subtotal = quantity * ppunit;
-	window.alert(subtotal);
+	tax = parseFloat(tax)/100.0;
+	discount = parseFloat(discount)/100.0;
 	
+	window.alert(tax);
+	window.alert(discount);
+	
+	var subtotal = quantity * ppunit;
+	
+	
+	var finalTotal = subtotal + (subtotal * tax);
+	
+	finalTotal = finalTotal - (finalTotal * discount);
+	finalTotal = finalTotal.toFixed(2);
+	
+	window.alert(finalTotal);
 	
 }
