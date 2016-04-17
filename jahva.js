9c30229f6c82dd12827b9e836d3ea6735d940926
iwillriot <william.diaz.102@my.csun.edu>
2016-04-17 05:59:13 -0700

    once the form is submitted the total value is calculated.
    There is a problem that the page gets refreshed once the calculations
    are done. How do I keep the total to remain on the form?

--- jahva.js
+++ jahva.js
@@ -1,25 +1,26 @@
 function run(){
+	//get variables form index.html
 	var quantity = document.getElementById("quantity").value;
 	var ppunit = document.getElementById("ppunit").value;
 	var tax = document.getElementById("tax").value;
 	var discount = document.getElementById("discount").value;
 	
-	
+	//convert the text variables to float for use in calculations
 	ppunit = parseFloat(ppunit);
 	tax = parseFloat(tax)/100.0;
 	discount = parseFloat(discount)/100.0;
 	
-	window.alert(tax);
-	window.alert(discount);
-	
+	//calculate the subtotal
 	var subtotal = quantity * ppunit;
 	
-	
+	//Add the tax to the final total
 	var finalTotal = subtotal + (subtotal * tax);
 	
+	//Subtract the tax from the final total	
 	finalTotal = finalTotal - (finalTotal * discount);
+	
+	//fix the final total to 2 decimal places
 	finalTotal = finalTotal.toFixed(2);
-	
-	window.alert(finalTotal);
-	
+
+	document.getElementById('total').value = finalTotal;
 }
