c1def5c24ce14b967c2bce5d7f8383cc5e479bfa
iwillriot <william.diaz.102@my.csun.edu>
2016-04-20 00:02:37 -0700

    Version 3. Final changes done. Ready for submission.

--- part1.js
+++ part1.js
@@ -3,23 +3,26 @@
 //Lab 6 - Part 1
 //****************
 
+//function to initiate the listener when the form is submitted
 function init() {
 	'use strict';
-	document.getElementById('theForm').onsubmit = run;
+	document.getElementById('theForm').onsubmit = calculate;
 }
-
+//load it on when the page loads
 window.onload = init;
 
-function run(){
+//this function calculates the total
+function calculate(){
 	'use strict';
+	
 	//get variables from the form in index.html
 	var quantity = document.getElementById("quantity").value;
-	var ppunit = document.getElementById("ppunit").value;
+	var ppunit = document.getElementById("ppunit").value; //this variable is the PRICE varible. 
 	var tax = document.getElementById("tax").value;
 	var discount = document.getElementById("discount").value;
 	
 	//convert the text variables to float for use in calculations
-	ppunit = parseFloat(ppunit);
+	ppunit = parseFloat(ppunit); //parsing the price
 	tax = parseFloat(tax)/100.0;
 	discount = parseFloat(discount)/100.0;
 	
@@ -35,6 +38,7 @@ function run(){
 	//fix the final total to 2 decimal places
 	finalTotal = finalTotal.toFixed(2);
 	
+	//place the finalTotal as the value of the input tag with the id of total
 	document.getElementById('total').value = finalTotal;
 	return false;
 }
--- part2.js
+++ part2.js
@@ -5,14 +5,20 @@
 
 function init() {
 	'use strict';
-	document.getElementById('theForm').onsubmit = toma;
+	document.getElementById('theForm').onsubmit = handleString;
 }
 window.onload = init;
 
-function toma() {
+function handleString() {
 	'use strict';
+	
+	//take in text as a string using the id of comment
 	var str = document.getElementById("comment").value;
+	
+	//split the string by character
 	str = str.split("");
+	
+	//set the max allowed characters from the value obtained from id of count
 	var max = document.getElementById("count").value;
 	
 	//parse it to int
@@ -26,8 +32,13 @@ function toma() {
 	
 	//loop and concatenate the string until the max given
 	for(i=0;i<max;i++){
+		
+		//build the string to output. one character at at time.
 		s = s + str[i];
+	
 	}
+	//set the string to the value of the input tag with id out.
 	document.getElementById("out").value = s;
+	
 	return false;
 }
